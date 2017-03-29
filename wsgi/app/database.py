8ddfbc18ce19052a50bf78d92ddd162b4302633d
gilmoregrills <robinfarrowyonge@gmail.com>
2017-03-29 11:31:21 +0100

    update database.py

--- wsgi/app/database.py
+++ wsgi/app/database.py
@@ -3,14 +3,14 @@ from flask_pymongo import PyMongo
 from flask import jsonify
 import os
 
-app.config['MONGO_DBNAME'] = os.environ['OPENSHIFT_APP_NAME']
+#app.config['MONGO_DBNAME'] = os.environ['OPENSHIFT_APP_NAME']
 app.config['MONGO_URI'] = os.environ['OPENSHIFT_MONGODB_DB_URL']
 
 mongo = PyMongo(app)
 
-#@app.route('/database/collections', methods=['GET'])
-#def get_all_databases():
-#    return jsonify({'result' : mongo.db.collection_names()})
+@app.route('/database/collections', methods=['GET'])
+def get_all_databases():
+    return jsonify({'result' : mongo.db.collection_names()})
 
 @app.route('/database/people', methods=['GET'])
 def get_all_people():
