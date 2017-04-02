8ef17f0c7a6d8098c932238e74856f0e147930a4
gilmoregrills <robinfarrowyonge@gmail.com>
2017-04-02 09:44:36 +0100

    more updates to comply with hand-in

--- wsgi/app/__init__.py
+++ wsgi/app/__init__.py
@@ -3,7 +3,7 @@ from flask import Flask
 app = Flask(__name__)
 
 #this is will be changed once app is deployed
-app.secret_key = 'localsecrets'
+app.secret_key = ''
 
 from app import views
 
--- wsgi/app/forms.py
+++ wsgi/app/forms.py
@@ -2,7 +2,7 @@ from flask_wtf import Form
 from wtforms import TextField, validators
 
 class MessageForm(Form):
-   message = TextField(u'tfw u realise that maybe u have spent too long online when u need a bot to tell u if a message is good or not', [validators.optional(), validators.length(max=200)])
+   message = TextField(u'tfw u realise that maybe u have spent too long online when u need a computer to tell u if a message is good or not', [validators.optional(), validators.length(max=200)])
 
 
 
